d63099fd65f13f8bcc1cce255bfbb827cb5358ce
oriKanner <orikanner12345@gmail.com>
2022-11-27 14:34:38 +0200

    fixed

--- src/App.js
+++ src/App.js
@@ -6,33 +6,10 @@ import ProductPage from "./components/ProductPage";
 import productListJSON from "./productListJSON.json";
 
 
+import {MyTitle,LinksBox,MyLink} from  "./publicDesign"
 
-export const MyTitle = styled.h1`
-  font-family: "Courier New", Courier, monospace;
-  display: flex;
-  flex-direction: column;
-  align-items:center;
 
-`
 
-const MyLink = styled.a`
-  margin: 1rem;
-  font-family: "Courier New", Courier, monospace;
-`;
-
-const LinksBox = styled.div`
-  
-  height: 50px;
-  width: 100%;
-  display: flex;
-  align-items:center;
-  position: relative;
-  padding: 0.004rem 0rem;
-  background-color: #fff;
-  color: black;
-  box-shadow: 0 2px 2px 2px rgba(9, 9, 9, 0.23);
-
-`;
 
 
 function App() {
---
+++ src/publicDesign.js
@@ -0,0 +1,27 @@
+import styled from "styled-components";
+
+export const MyTitle = styled.h1`
+  font-family: "Courier New", Courier, monospace;
+  display: flex;
+  flex-direction: column;
+  align-items:center;
+
+`
+export const MyLink = styled.a`
+  margin: 1rem;
+  font-family: "Courier New", Courier, monospace;
+`;
+
+export const LinksBox = styled.div`
+  
+  height: 50px;
+  width: 100%;
+  display: flex;
+  align-items:center;
+  position: relative;
+  padding: 0.004rem 0rem;
+  background-color: #fff;
+  color: black;
+  box-shadow: 0 2px 2px 2px rgba(9, 9, 9, 0.23);
+
+`;
